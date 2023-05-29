7930ae3a2d3ee920bcdacaae1b4bf4d3c994a361
mmutawe <mahmoudmutawe@gmail.com>
2023-05-29 08:40:40 -0500

    Initial commit

---
+++ src/test/java/com/mmutawe/projects/chatgpt/bot/ChatgptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmutawe.projects.chatgpt.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
